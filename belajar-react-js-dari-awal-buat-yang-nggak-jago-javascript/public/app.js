04223080bccabeb3901ece340e782abe535b41cd
dimassdoubles <codedimas@gmail.com>
2023-08-28 06:49:32 +0700

    10/40 - React list & key

--- belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/public/app.js
+++ belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/public/app.js
@@ -1,144 +1,17 @@
 const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
 
-// =====
-// ===== COMPONENT LIFECYCLE =====
-// =====
-// function Welcome() {
-//     const welcomeStyle = {
-//         color: "blue",
-//         fontSize: "23px"
-//     }
-//     return <h1 style={welcomeStyle}>Hello World!</h1>
-// }
-
-// const sayHello = () => console.log("Hello World!");
-// const sebutNamaku = (namaku) => alert(namaku);
-
-// function MyButton() {
-//     // return <button onClick={function() {sebutNamaku("bento")}}>Say Hello</button>
-//     return <button onClick={sebutNamaku.bind(this, "bento")}>Say Hello</button>
-// }
-
-// function MyComponent() {
-//     const state = React.useState(1);
-
-//     return <h1>{state}</h1>
-// }
-
-// function MyCounter() {
-//     // const state = React.useState(0); 
-
-//     // const count = state[0];
-//     // const updateCount = state[1];
-
-//     // karena useState mengembalikan array
-//     // untuk membuat state bisa juga menggunakan 
-//     // konsep destructuring array
-//     const [count, setCount] = React.useState(0);
-
-//     return (
-//         <>
-//             <button onClick={setCount.bind(this, count-1)}>-</button>
-//             <span> {count} </span>
-//             <button onClick={setCount.bind(this, count+1)}>+</button>
-//         </>
-//     );
-
-// }
-
-// // component lifecycle
-// function ComponentLifecycle() {
-//     const [diklik, setDiklik] = React.useState(false);
-//     const [count, setCount] = React.useState(0);
-
-//     // component did mount = render pertama kali
-//     // React.useEffect(function() {}, []); => useEffect hanya akan dijalankan satu kali saja
-
-//     // component did update = ketika komponen di update
-//     React.useEffect(function() { // function ini akan dipanggil saat element dirender
-//         console.log("component init");
-
-//         // component unmount
-//         // function ini akan dieksekusi jika
-//         // komponen akan dihapus dari dom tree
-//         // atau saat komponen dirender ulang (saat useEffect di eksekusi)
-//         return function() {
-//             console.log("destroy comopnent");
-//         };
-//     }, [count, diklik]); // argument kedua, mengatur fungsi akan dipanggil saat state x berubah
-
-//     return (
-//         <>
-//             <h1 id="mama">Hello World!</h1>
-//             <button onClick={function() {
-//                 setDiklik(!diklik);
-//             }}>Klik aku dong</button>
-//             <button onClick={function() {
-//                 setCount(count + 1);
-//             }}>Tambah</button>
-//             Nilai saat ini: {count}
-//         </>
-//     )
-// }
-
-// const element = <ComponentLifecycle/>;
-
-// =====
-// ===== CONDITIONAL RENDERING
-// =====
-
-// function App() {
-
-//     const [login, setLogin] = React.useState(false);
-
-//     // if (login) {
-//     //     return (
-//     //         <>
-//     //             <h1>Udah login, Bang</h1>
-//     //             <button onClick={function() {
-//     //                 setLogin(false );
-//     //             }}>Logoout</button>
-//     //         </>
-//     //     );
-//     // }
-
-//     return (
-//         <>
-//             <h1>Application</h1>
-//              {login && <h2>Udah login, Bang</h2>}
-//              <button onClick={function() {
-//                 setLogin(!login);
-//              }}>{login ? 'Logout' : 'Login'}</button>
-//         </>
-//     );
-// }
-
 // ===
-// === DOM MANIPULATION
+// === REACT LIST
 // ===
 
 function App() {
-  // React.useEffect(function() {
-  //     const judul = document.getElementById('judul');
-  //     setTimeout(function() {
-  //         judul.textContent = "Aplikasi";
-  //     }, 1000);
-  // }, []);
-
-  // return <h1 id="judul">Application</h1>
-
-  // ==== kode di atas bisa diganti dengan
-  const judulRef = React.useRef(null);
-  React.useEffect(function () {
-    console.log(judulRef);
-    setTimeout(function () {
-      judulRef.current.textContent = "Aplikasi";
-    }, 1000);
-  });
-  return /*#__PURE__*/React.createElement("h1", {
-    ref: judulRef
-  }, "Application");
+  const fruits = ['Apple', 'Orang', 'Grape', 'Lengkeng'];
+  return /*#__PURE__*/React.createElement("ul", null, fruits.map(function (fruit) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: fruits
+    }, fruit);
+  }));
 }
 const element = /*#__PURE__*/React.createElement(App, null);
 root.render(element);
