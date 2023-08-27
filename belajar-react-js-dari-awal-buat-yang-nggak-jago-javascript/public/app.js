b7d0e20008fc9a35bbc0bf70af8701facc356125
dimassdoubles <codedimas@gmail.com>
2023-08-27 10:36:03 +0700

    8/40 Belajar react

--- belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/public/app.js
+++ belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/public/app.js
@@ -1,64 +1,95 @@
 const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
-function Welcome() {
-  const welcomeStyle = {
-    color: "blue",
-    fontSize: "23px"
-  };
-  return /*#__PURE__*/React.createElement("h1", {
-    style: welcomeStyle
-  }, "Hello World!");
-}
-const sayHello = () => console.log("Hello World!");
-const sebutNamaku = namaku => alert(namaku);
-function MyButton() {
-  // return <button onClick={function() {sebutNamaku("bento")}}>Say Hello</button>
-  return /*#__PURE__*/React.createElement("button", {
-    onClick: sebutNamaku.bind(this, "bento")
-  }, "Say Hello");
-}
-function MyComponent() {
-  const state = React.useState(1);
-  return /*#__PURE__*/React.createElement("h1", null, state);
-}
-function MyCounter() {
-  // const state = React.useState(0); 
 
-  // const count = state[0];
-  // const updateCount = state[1];
+// =====
+// ===== COMPONENT LIFECYCLE =====
+// =====
+// function Welcome() {
+//     const welcomeStyle = {
+//         color: "blue",
+//         fontSize: "23px"
+//     }
+//     return <h1 style={welcomeStyle}>Hello World!</h1>
+// }
 
-  // karena useState mengembalikan array
-  // untuk membuat state bisa juga menggunakan 
-  // konsep destructuring array
-  const [count, setCount] = React.useState(0);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("button", {
-    onClick: setCount.bind(this, count - 1)
-  }, "-"), /*#__PURE__*/React.createElement("span", null, " ", count, " "), /*#__PURE__*/React.createElement("button", {
-    onClick: setCount.bind(this, count + 1)
-  }, "+"));
+// const sayHello = () => console.log("Hello World!");
+// const sebutNamaku = (namaku) => alert(namaku);
+
+// function MyButton() {
+//     // return <button onClick={function() {sebutNamaku("bento")}}>Say Hello</button>
+//     return <button onClick={sebutNamaku.bind(this, "bento")}>Say Hello</button>
+// }
+
+// function MyComponent() {
+//     const state = React.useState(1);
+
+//     return <h1>{state}</h1>
+// }
+
+// function MyCounter() {
+//     // const state = React.useState(0); 
+
+//     // const count = state[0];
+//     // const updateCount = state[1];
+
+//     // karena useState mengembalikan array
+//     // untuk membuat state bisa juga menggunakan 
+//     // konsep destructuring array
+//     const [count, setCount] = React.useState(0);
+
+//     return (
+//         <>
+//             <button onClick={setCount.bind(this, count-1)}>-</button>
+//             <span> {count} </span>
+//             <button onClick={setCount.bind(this, count+1)}>+</button>
+//         </>
+//     );
+
+// }
+
+// // component lifecycle
+// function ComponentLifecycle() {
+//     const [diklik, setDiklik] = React.useState(false);
+//     const [count, setCount] = React.useState(0);
+
+//     // component did mount = render pertama kali
+//     // React.useEffect(function() {}, []); => useEffect hanya akan dijalankan satu kali saja
+
+//     // component did update = ketika komponen di update
+//     React.useEffect(function() { // function ini akan dipanggil saat element dirender
+//         console.log("component init");
+
+//         // component unmount
+//         // function ini akan dieksekusi jika
+//         // komponen akan dihapus dari dom tree
+//         // atau saat komponen dirender ulang (saat useEffect di eksekusi)
+//         return function() {
+//             console.log("destroy comopnent");
+//         };
+//     }, [count, diklik]); // argument kedua, mengatur fungsi akan dipanggil saat state x berubah
+
+//     return (
+//         <>
+//             <h1 id="mama">Hello World!</h1>
+//             <button onClick={function() {
+//                 setDiklik(!diklik);
+//             }}>Klik aku dong</button>
+//             <button onClick={function() {
+//                 setCount(count + 1);
+//             }}>Tambah</button>
+//             Nilai saat ini: {count}
+//         </>
+//     )
+// }
+
+// const element = <ComponentLifecycle/>;
+
+// =====
+// ===== CONDITIONAL RENDERING
+// =====
+
+function App() {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Login dulu, Bang"), /*#__PURE__*/React.createElement("button", null, "Login"));
 }
-
-// component lifecycle
-function ComponentLifecycle() {
-  const [diklik, setDiklik] = React.useState(false);
-  const [count, setCount] = React.useState(0);
-  React.useEffect(function () {
-    // function ini akan dipanggil saat element dirender
-    console.log(document.getElementById('mama'));
-    console.log("Mama aku dirender");
-  }, [count]); // argument kedua, mengatur fungsi akan dipanggil saat state x berubah
-
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
-    id: "mama"
-  }, "Hello World!"), /*#__PURE__*/React.createElement("button", {
-    onClick: function () {
-      setDiklik(!diklik);
-    }
-  }, "Klik aku dong"), /*#__PURE__*/React.createElement("button", {
-    onClick: function () {
-      setCount(count + 1);
-    }
-  }, "Tambah"), "Nilai saat ini: ", count);
-}
-const element = /*#__PURE__*/React.createElement(ComponentLifecycle, null);
+const element = /*#__PURE__*/React.createElement(App, null);
 root.render(element);
--- belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/src/app.js
+++ belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/src/app.js
@@ -1,75 +1,105 @@
 const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
 
-function Welcome() {
-    const welcomeStyle = {
-        color: "blue",
-        fontSize: "23px"
-    }
-    return <h1 style={welcomeStyle}>Hello World!</h1>
-}
 
-const sayHello = () => console.log("Hello World!");
-const sebutNamaku = (namaku) => alert(namaku);
+// =====
+// ===== COMPONENT LIFECYCLE =====
+// =====
+// function Welcome() {
+//     const welcomeStyle = {
+//         color: "blue",
+//         fontSize: "23px"
+//     }
+//     return <h1 style={welcomeStyle}>Hello World!</h1>
+// }
 
-function MyButton() {
-    // return <button onClick={function() {sebutNamaku("bento")}}>Say Hello</button>
-    return <button onClick={sebutNamaku.bind(this, "bento")}>Say Hello</button>
-}
+// const sayHello = () => console.log("Hello World!");
+// const sebutNamaku = (namaku) => alert(namaku);
 
-function MyComponent() {
-    const state = React.useState(1);
+// function MyButton() {
+//     // return <button onClick={function() {sebutNamaku("bento")}}>Say Hello</button>
+//     return <button onClick={sebutNamaku.bind(this, "bento")}>Say Hello</button>
+// }
 
-    return <h1>{state}</h1>
-}
+// function MyComponent() {
+//     const state = React.useState(1);
 
-function MyCounter() {
-    // const state = React.useState(0); 
+//     return <h1>{state}</h1>
+// }
+
+// function MyCounter() {
+//     // const state = React.useState(0); 
     
-    // const count = state[0];
-    // const updateCount = state[1];
+//     // const count = state[0];
+//     // const updateCount = state[1];
     
-    // karena useState mengembalikan array
-    // untuk membuat state bisa juga menggunakan 
-    // konsep destructuring array
-    const [count, setCount] = React.useState(0);
+//     // karena useState mengembalikan array
+//     // untuk membuat state bisa juga menggunakan 
+//     // konsep destructuring array
+//     const [count, setCount] = React.useState(0);
 
+//     return (
+//         <>
+//             <button onClick={setCount.bind(this, count-1)}>-</button>
+//             <span> {count} </span>
+//             <button onClick={setCount.bind(this, count+1)}>+</button>
+//         </>
+//     );
+
+// }
+
+// // component lifecycle
+// function ComponentLifecycle() {
+//     const [diklik, setDiklik] = React.useState(false);
+//     const [count, setCount] = React.useState(0);
+
+//     // component did mount = render pertama kali
+//     // React.useEffect(function() {}, []); => useEffect hanya akan dijalankan satu kali saja
+
+
+//     // component did update = ketika komponen di update
+//     React.useEffect(function() { // function ini akan dipanggil saat element dirender
+//         console.log("component init");
+
+//         // component unmount
+//         // function ini akan dieksekusi jika
+//         // komponen akan dihapus dari dom tree
+//         // atau saat komponen dirender ulang (saat useEffect di eksekusi)
+//         return function() {
+//             console.log("destroy comopnent");
+//         };
+//     }, [count, diklik]); // argument kedua, mengatur fungsi akan dipanggil saat state x berubah
+
+
+//     return (
+//         <>
+//             <h1 id="mama">Hello World!</h1>
+//             <button onClick={function() {
+//                 setDiklik(!diklik);
+//             }}>Klik aku dong</button>
+//             <button onClick={function() {
+//                 setCount(count + 1);
+//             }}>Tambah</button>
+//             Nilai saat ini: {count}
+//         </>
+//     )
+// }
+
+// const element = <ComponentLifecycle/>;
+
+// =====
+// ===== CONDITIONAL RENDERING
+// =====
+
+function App() {
     return (
         <>
-            <button onClick={setCount.bind(this, count-1)}>-</button>
-            <span> {count} </span>
-            <button onClick={setCount.bind(this, count+1)}>+</button>
+             <h1>Login dulu, Bang</h1>
+             <button>Login</button>
         </>
     );
-
 }
 
-// component lifecycle
-function ComponentLifecycle() {
-    const [diklik, setDiklik] = React.useState(false);
-    const [count, setCount] = React.useState(0);
-
-    React.useEffect(function() { // function ini akan dipanggil saat element dirender
-        console.log(document.getElementById('mama'));
-        console.log("Mama aku dirender");
-    }, [count]); // argument kedua, mengatur fungsi akan dipanggil saat state x berubah
-
-
-    return (
-        <>
-            <h1 id="mama">Hello World!</h1>
-            <button onClick={function() {
-                setDiklik(!diklik);
-            }}>Klik aku dong</button>
-            <button onClick={function() {
-                setCount(count + 1);
-            }}>Tambah</button>
-            Nilai saat ini: {count}
-        </>
-    )
-}
-
-const element = <ComponentLifecycle/>;
-
+const element = <App/>
 
 root.render(element);
