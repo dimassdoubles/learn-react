aff16b2730fec2891fd853136260f55c4bc1b1d9
dimassdoubles <codedimas@gmail.com>
2023-08-28 07:11:07 +0700

    10/40 - Forms

--- belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/public/app.js
+++ belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/public/app.js
@@ -2,16 +2,37 @@ const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
 
 // ===
-// === REACT LIST
+// === REACT FORM
 // ===
 
 function App() {
-  const fruits = ['Apple', 'Orang', 'Grape', 'Lengkeng'];
-  return /*#__PURE__*/React.createElement("ul", null, fruits.map(function (fruit) {
-    return /*#__PURE__*/React.createElement("li", {
-      key: fruits
-    }, fruit);
-  }));
+  // const namaRef = React.useRef(null); => uncontrolled component
+
+  const [nama, setNama] = React.useState('Dimas Saputro');
+  function ketikaSubmit(event) {
+    event.preventDefault(); // mencegah pindah halaman ketika form di submit
+    console.log("Aku disubmit, mama");
+
+    // const nama = namaRef.current.value;
+    // console.log("Namaku: ", nama);
+
+    console.log({
+      nama
+    });
+  }
+  return /*#__PURE__*/React.createElement("form", {
+    onSubmit: ketikaSubmit
+  }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Nama: "), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "nama",
+    value: nama,
+    onChange: function (event) {
+      console.log(event.target.value);
+      setNama(event.target.value);
+    }
+  })), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, "Kirim"));
 }
 const element = /*#__PURE__*/React.createElement(App, null);
 root.render(element);
--- belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/src/app.js
+++ belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/src/app.js
@@ -2,25 +2,40 @@ const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
 
 // ===
-// === REACT LIST
+// === REACT FORM
 // ===
 
 function App() {
-    const fruits = [
-        'Apple',
-        'Orang',
-        'Grape',
-        'Lengkeng',
-    ]
+
+    // const namaRef = React.useRef(null); => uncontrolled component
+
+    const [nama, setNama] = React.useState('Dimas Saputro');
+
+    function ketikaSubmit(event) {
+        event.preventDefault(); // mencegah pindah halaman ketika form di submit
+        console.log("Aku disubmit, mama");
+
+        // const nama = namaRef.current.value;
+        // console.log("Namaku: ", nama);
+
+        console.log({nama});
+
+    }
 
     return (
-        <ul>
-        
-            {fruits.map(function(fruit) {
-                return <li key={fruits}>{fruit}</li>;
-            })}
-        
-        </ul>
+        <form onSubmit={ketikaSubmit}>
+            <div>
+                <label>Nama: </label>
+                {/* <input type="text" name="nama" ref={namaRef}> */}
+                <input type="text" name="nama" value={nama} onChange={function(event) {
+                    console.log(event.target.value);
+
+                    setNama(event.target.value);
+                }}>
+                </input>
+            </div>
+            <button type="submit">Kirim</button>
+        </form>
     );
 }
 
