b1f51526b78c10e125477ee87a22694f34da98ff
dimassdoubles <codedimas@gmail.com>
2023-08-28 15:44:37 +0700

    13/40   Final project todo list app

--- belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/public/app.js
+++ belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/public/app.js
@@ -10,6 +10,9 @@ const root = ReactDOM.createRoot(container);
 function App() {
   const [activity, setActivity] = React.useState("");
   const [todoList, setTodoList] = React.useState([]);
+  function generateId() {
+    return Date.now();
+  }
   function addTodoHandler(event) {
     event.preventDefault();
     console.log({
@@ -18,16 +21,28 @@ function App() {
     console.log({
       todoList
     });
-    setTodoList([...todoList, activity]);
+    setTodoList([...todoList, {
+      id: generateId(),
+      activity: activity
+    }]);
     console.log({
       todoList
     });
+    setActivity("");
+  }
+  function removeTodoHandler(id) {
+    const filteredTodos = todoList.filter(function (item) {
+      return item.id != id;
+    });
+    setTodoList(filteredTodos);
+    console.log(todoList);
   }
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Simple Todo List"), /*#__PURE__*/React.createElement("form", {
     onSubmit: addTodoHandler
   }, /*#__PURE__*/React.createElement("input", {
     type: "text",
     placeholder: "Nama aktifitas",
+    value: activity,
     onChange: function (event) {
       setActivity(event.target.value);
     }
@@ -35,8 +50,10 @@ function App() {
     type: "submit"
   }, "Tambah")), /*#__PURE__*/React.createElement("ul", null, todoList.map(function (item) {
     return /*#__PURE__*/React.createElement("li", {
-      key: item
-    }, item);
+      key: item.id
+    }, item.activity, /*#__PURE__*/React.createElement("button", {
+      onClick: removeTodoHandler.bind(this, item.id)
+    }, "Hapus"));
   })));
 }
 const element = /*#__PURE__*/React.createElement(App, null);
--- belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/src/app.js
+++ belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/src/app.js
@@ -12,20 +12,38 @@ function App() {
     const [activity, setActivity] = React.useState("");
     const [todoList, setTodoList] = React.useState([]);
 
+    function generateId() {
+        return Date.now();
+    }
+
     function addTodoHandler(event) {
         event.preventDefault();
         console.log({activity});
         console.log({todoList});
 
-        setTodoList([...todoList, activity]);
+        setTodoList([...todoList, {
+            id: generateId(),
+            activity: activity,
+        }]);
         console.log({todoList});
+        
+        setActivity("");
+    }
+
+    function removeTodoHandler(id) {
+        const filteredTodos = todoList.filter(function(item) {
+            return item.id != id;
+        })
+
+        setTodoList(filteredTodos);
+        console.log(todoList);
     }
 
     return (
         <>
             <h1>Simple Todo List</h1>
             <form onSubmit={addTodoHandler}>
-                <input type="text" placeholder="Nama aktifitas" onChange={function(event) {
+                <input type="text" placeholder="Nama aktifitas" value={activity} onChange={function(event) {
                     setActivity(event.target.value);
                 }}></input>
                 <button type="submit">Tambah</button>
@@ -33,7 +51,12 @@ function App() {
 
             <ul>
                 {todoList.map(function(item) {
-                    return <li key={item}>{item}</li>
+                    return (
+                        <li key={item.id}>
+                            {item.activity} 
+                            <button onClick={removeTodoHandler.bind(this, item.id)}>Hapus</button>
+                        </li> 
+                    );
                 })}
             </ul>
         </>
