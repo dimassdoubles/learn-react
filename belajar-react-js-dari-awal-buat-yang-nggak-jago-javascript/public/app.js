1a35f4473ca2fa31bba63d46097cd594b3abbef9
dimassdoubles <codedimas@gmail.com>
2023-08-28 14:03:30 +0700

    12/40 - Data fetch (http cliet request)

--- belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/public/app.js
+++ belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/public/app.js
@@ -8,14 +8,33 @@ const root = ReactDOM.createRoot(container);
 // https://api.spaceflightnewsapi.net/v3/blogs
 
 function App() {
+  const [news, setNews] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
   React.useEffect(function () {
-    const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs').then(function (response) {
-      console.log(typeof response);
+    // const getData = fetch(('https://api.spaceflightnewsapi.net/v3/blogs').then(
+    //     function(response) {
+    //         return response.json();
+    //     }
+    // ).then(function(json) {
+    //     console.log(json);
+    // });
+    // console.log(getData);
+
+    async function getData() {
+      const request = await fetch('https://api.spaceflightnewsapi.net/v3/blogs');
+      console.log(request);
+      const response = await request.json();
       console.log(response);
-    });
-    console.log(getData);
+      setNews(response);
+      setLoading(false);
+    }
+    getData();
   }, []);
-  return /*#__PURE__*/React.createElement("div", null, "Data Fetch");
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", null, "Data Fetch"), /*#__PURE__*/React.createElement("ul", null, news.map(function (item) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: item.id
+    }, item.title);
+  })));
 }
 const element = /*#__PURE__*/React.createElement(App, null);
 root.render(element);
--- belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/src/app.js
+++ belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/src/app.js
@@ -8,17 +8,43 @@ const root = ReactDOM.createRoot(container);
 // https://api.spaceflightnewsapi.net/v3/blogs
 
 function App() {
+
+    const [news, setNews] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
+
     React.useEffect(function() {
-        const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs').then(
-            function(response) {
-                console.log(typeof response);
-                console.log(response);
-            }
-        );
-        console.log(getData);
+        // const getData = fetch(('https://api.spaceflightnewsapi.net/v3/blogs').then(
+        //     function(response) {
+        //         return response.json();
+        //     }
+        // ).then(function(json) {
+        //     console.log(json);
+        // });
+        // console.log(getData);
+
+        async function getData() {
+            const request = await fetch('https://api.spaceflightnewsapi.net/v3/blogs');
+            console.log(request);
+
+            const response = await request.json();
+            console.log(response);
+
+            setNews(response);
+            setLoading(false);
+        }
+        getData();
     }, []);
 
-    return <div>Data Fetch</div>;
+    return (
+        <>
+            <div>Data Fetch</div>
+            <ul>
+                {news.map(function(item) {
+                    return <li key={item.id}>{item.title}</li>;
+                })}
+            </ul>
+        </> 
+    );
 
 }
 
