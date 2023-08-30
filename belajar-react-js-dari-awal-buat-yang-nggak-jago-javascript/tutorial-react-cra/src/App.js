0da6a513cc92c78f8cf147bd4acc112dd1dcc924
dimassdoubles <codedimas@gmail.com>
2023-08-31 06:28:52 +0700

    20/40 = Static file

--- belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/tutorial-react-cra/src/App.js
+++ belajar-react-js-dari-awal-buat-yang-nggak-jago-javascript/tutorial-react-cra/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="/logo512.png" alt="Logo 512"></img>
         <Button>Hello World!</Button>
       </header>
     </div>
